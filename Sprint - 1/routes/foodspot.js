24978b0b69e13c670264c04309e9376cffce7e8b
Mishab <mishabmsb91@gmail.com>
2023-11-03 15:43:59 +0530

    user sign up

--- Sprint - 1/routes/foodspot.js
+++ Sprint - 1/routes/foodspot.js
@@ -24,17 +24,42 @@ router.route("/signup")
       res.redirect("/foodspots");
     } else {
       res.render("foodspots/signup", {
-        foodspot: true,layout:'emptylayout',
+        foodspot: true,
+        layout: 'emptylayout',
         signUpErr: req.session.signUpErr,
       });
     }
   })
   .post(function (req, res) {
+    const phoneRegex = /^\d{10}$/; // Match 10-digit phone numbers
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // Basic email validation
+
+    const { Phone, Email, Password } = req.body;
+
+    // Validate phone number
+    if (!Phone.match(phoneRegex)) {
+      const error = "Invalid phone number (10 digits required)";
+      return sendValidationAlert(res, error);
+    }
+
+    // Validate email
+    if (!Email.match(emailRegex)) {
+      const error = "Invalid email address";
+      return sendValidationAlert(res, error);
+    }
+
+    // Validate password length
+    if (Password.length < 6) {
+      const error = "Password must be at least 6 characters long";
+      return sendValidationAlert(res, error);
+    }
+
+    // If all validations pass, proceed with signup
     foodspotHelper.doSignup(req.body).then((response) => {
       console.log(response);
       if (response.status == false) {
         req.session.signUpErr = "Invalid Code";
-        res.redirect("/foodspots/signup");
+        return sendValidationAlert(res, "Invalid Code");
       } else {
         req.session.signedInFoodspot = true;
         req.session.foodspot = response;
@@ -42,6 +67,16 @@ router.route("/signup")
       }
     });
   });
+
+function sendValidationAlert(res, message) {
+  res.setHeader('Content-Type', 'text/html');
+  res.status(400).send(`
+    <script>
+      alert("${message}");
+      window.history.back();
+    </script>
+  `);
+}
 
 
   router.route("/signin")
--- Sprint - 1/routes/users.js
+++ Sprint - 1/routes/users.js
@@ -29,13 +29,66 @@ router.get("/signup", function (req, res) {
   }
 });
 
-router.post("/signup", function (req, res) {
-  userHelper.doSignup(req.body).then((response) => {
-    req.session.signedIn = true;
-    req.session.user = response;
-    res.redirect("/");
+router.route("/signup")
+  .get(function (req, res) {
+    if (req.session.signedIn) {
+      res.redirect("/");
+    } else {
+      res.render("/signup", {
+        admin: false,
+        layout: 'emptylayout',
+        signUpErr: req.session.signUpErr,
+      });
+    }
+  })
+  .post(function (req, res) {
+    const phoneRegex = /^\d{10}$/; // Match 10-digit phone numbers
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // Basic email validation
+
+    const { Phone, Email, Password } = req.body;
+
+    // Validate phone number
+    if (!Phone.match(phoneRegex)) {
+      const error = "Invalid phone number (10 digits required)";
+      return sendValidationAlert(res, error);
+    }
+
+    // Validate email
+    if (!Email.match(emailRegex)) {
+      const error = "Invalid email address";
+      return sendValidationAlert(res, error);
+    }
+
+    // Validate password length
+    if (Password.length < 6) {
+      const error = "Password must be at least 6 characters long";
+      return sendValidationAlert(res, error);
+    }
+
+    // If all validations pass, proceed with signup
+    userHelper.doSignup(req.body).then((response) => {
+      console.log(response);
+      if (response.status == false) {
+        req.session.signUpErr = "Invalid Code";
+        return sendValidationAlert(res, "Invalid Code");
+      } else {
+        req.session.signedIn = true;
+        req.session.user = response;
+        res.redirect("/");
+      }
+    });
   });
-});
+
+  function sendValidationAlert(res, message) {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(400).send(`
+      <script>
+        alert("${message}");
+        window.history.back();
+      </script>
+    `);
+  }
+  
 
 router.get("/signin", function (req, res) {
   if (req.session.signedIn) {
